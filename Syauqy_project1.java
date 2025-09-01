d505ef881e6460926523e478fac1e49b4223ed45
AmdSyauqyy <ahmadsyauqy098@gmail.com>
2025-09-01 23:32:01 +0700

    Add files via upload

---
+++ Syauqy_project1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.syauqy_project1;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Syauqy_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama : Ahmad Syauqy \nNpm : 2440506051 \nAlamat : Jawa Selatan \nNo HP : 999");
+    }
+}
